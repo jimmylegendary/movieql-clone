35090e96053c49333135c22f42f90b9d8114c865
SeunghoLee <jimmylee871124@gmail.com>
2019-02-07 19:40:58 +0900

    Use db from url and modify all the functions to use new db

--- graphql/db.js
+++ graphql/db.js
@@ -1,51 +1,16 @@
-let movies = [
-  {
-    id: "0",
-    name: "Star Wars - The new one",
-    score: 1
-  },
-  {
-    id: "1",
-    name: "Avengers - The new one",
-    score: 8
-  },
-  {
-    id: "2",
-    name: "The Godfather I",
-    score: 99
-  },
-  {
-    id: "3",
-    name: "Loga",
-    score: 2
+import fetch from "node-fetch";
+
+const API_URL = "https://yts.am/api/v2/list_movies.json?"
+
+export const getMovies = (limit, rating) => {
+  let REQUEST_URL = API_URL;
+  if(limit > 0){
+    REQUEST_URL += `limit=${limit}`;
   }
-];
-
-export const getMovies = () => movies;
-
-export const getById = id => {
-  const filteredMovies = movies.filter(movie => movie.id === String(id));
-  return filteredMovies[0];
-}
-
-export const deleteMovie = (id) => {
-  const cleanedMovies = movies.filter(movie => movie.id !== String(id));
-  if(movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else {
-    return false;
+  if(rating > 0){
+    REQUEST_URL += `&minimum_rating=${rating}`;
   }
-}
-
-export const addMovie = (name, score) => {
-  const newId = String(movies.length);
-  const newMovie = {
-    id: newId,
-    name,
-    score
-  };
-  movies.push(newMovie);
-  console.log(movies);
-  return newMovie;
+  return fetch(REQUEST_URL)
+  .then(res => res.json())
+  .then(json => json.data.movies);
 }
